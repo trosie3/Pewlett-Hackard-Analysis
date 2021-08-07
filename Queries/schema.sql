9afdc4c4e07ff1dae79f6f998df29e561bb1c514
trosie3 <79772644+trosie3@users.noreply.github.com>
2021-08-07 10:19:11 -0700

    schema update

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -51,7 +51,6 @@ CREATE TABLE titles (
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
 );
 
 --Query for comfirmation
